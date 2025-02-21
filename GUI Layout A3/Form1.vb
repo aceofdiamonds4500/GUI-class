d7f6dbbf88001e714b451f245484e123eb9ded0c
Nicholas <45800061+aceofdiamonds4500@users.noreply.github.com>
2025-02-20 21:21:37 -0500

    Assignment A3

---
+++ GUI Layout A3/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
+
+	Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+	End Sub
+
+	Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+	End Sub
+End Class
